cdf6d78dd3dc072e587a48659bb160bf64bc78d1
mekaala <mekaala@gmail.com>
2020-02-24 11:11:41 -0500

    pulls up seed files on server

--- controllers/photo.js
+++ controllers/photo.js
@@ -4,15 +4,7 @@ const Region = require('../models/region.js');
 const Area = require('../models/area.js');
 const photoRouter = express.Router()
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
 
 photoRouter.get('/', (req, res) => {
   Atlanta.find().then(atlanta => {
@@ -20,9 +12,4 @@ photoRouter.get('/', (req, res) => {
   });
 });
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
 module.exports = photoRouter;
