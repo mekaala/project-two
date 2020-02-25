f537655be8b90d4fe961e8be6f31d383c2f9f346
mekaala <mekaala@gmail.com>
2020-02-25 11:00:04 -0500

    pulls up home page and region information

--- controllers/photo.js
+++ controllers/photo.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Atlanta = require('../models/atlanta.js');
-const Region = require('../models/region.js');
-const Area = require('../models/area.js');
+const Region = require('../models/Region.js');
+const Area = require('../models/Area.js');
 const photoRouter = express.Router()
 
 
@@ -12,10 +12,18 @@ photoRouter.get('/', (req, res) => {
   });
 });
 
-photoRouter.get('/atlanta/:atlantaId', (req, res) => {
-  Region.findById(req.params.atlantaId).then(region => {
+photoRouter.get('/:atlantaId', (req, res) => {
+  Atlanta.findById(req.params.atlantaId).then(region => {
       res.render('photos/region', { region });
   });
 });
 
+photoRouter.get('/new', (req, res) => {
+  res.render('photos/newRegionForm');
+});
+photoRouter.post('/', (req, res) => {
+  Atlanta.create(req.body).then(() => {
+      res.redirect('/atlanta');
+  });
+});
 module.exports = photoRouter;
