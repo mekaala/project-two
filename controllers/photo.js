0d390a103798772017d63f1a78d0cfc850ac1bfc
mekaala <mekaala@gmail.com>
2020-02-25 17:19:00 -0500

    remade my get methods according to new object layout

--- controllers/photo.js
+++ controllers/photo.js
@@ -7,23 +7,54 @@ const photoRouter = express.Router()
 
 
 photoRouter.get('/', (req, res) => {
-  Atlanta.find().then(atlanta => {
+  Area.find().then(atlanta => {
       res.render('photos/atlanta', { atlanta });
   });
 });
 
-photoRouter.get('/:atlantaId', (req, res) => {
-  Atlanta.findById(req.params.atlantaId).then(region => {
-      res.render('photos/region', { region });
+photoRouter.get('/:regionId', (req, res) => {
+  Area.find({region: req.params.regionId}).then(areas => {
+    console.log(areas);
+    res.render('photos/region', { areas });
+  })
   });
-});
+
+photoRouter.get('/area/:areaId', (req, res) => {
+  Area.findById(req.params.areaId).then(area => {
+    res.render('photos/area', { area })
+  })
+})
 
 photoRouter.get('/new', (req, res) => {
   res.render('photos/newRegionForm');
 });
 photoRouter.post('/', (req, res) => {
-  Atlanta.create(req.body).then(() => {
+  Area.create(req.body).then(() => {
       res.redirect('/atlanta');
   });
 });
+
+// photoRouter.get('/:atlantaId/new', (req, res) => {
+//   res.render('photos/newAreaForm');
+// });
+// photoRouter.post('/', (req, res) => {
+//   Atlanta.create(req.body).then(() => {
+//       res.redirect('/atlanta');
+//   });
+// });
+
+
+// photoRouter.get('/:atlantaId', (req, res) => {
+//   Atlanta.findById(req.params.atlantaId).populate('area').then(area => {
+//     res.send(area)
+//       // res.render('photos/area', { area });
+//   });
+// });
+
+
+// photoRouter.get('/:atlantaId', (req, res) => {
+//   Area.find({region: req.params.atlantaId}).then(areas => {
+//     res.render(areas)
+//   }
+// })
 module.exports = photoRouter;
