aaec3bc158fcbacaa1f7379700ee88357575470b
mekaala <mekaala@gmail.com>
2020-02-26 16:25:58 -0500

    created new methods

--- controllers/area.js
+++ controllers/area.js
@@ -2,12 +2,21 @@ const express = require('express')
 const Area = require('../models/Area.js')
 const areaRouter = express.Router()
 
+// ==============
+// INDEX OF AREAS
+// ==============
+
 areaRouter.get('/:name', (req, res) => {
   Area.find({name: req.params.name}).then(areas => {
     console.log(areas);
     res.render('areas/areas', { areas });
   })
-  });
+});
+
+
+// ================
+// SHOW SINGLE AREA
+// ================
 
 areaRouter.get('/area/:areaId', (req, res) => {
   Area.findById(req.params.areaId).then(area => {
@@ -15,28 +24,37 @@ areaRouter.get('/area/:areaId', (req, res) => {
   })
 })
 
+// ===============
+// CREATE NEW AREA
+// ===============
+
 areaRouter.get('/create/new', (req, res) => {
   res.render('areas/newAreaForm');
 });
-areaRouter.post('/', (req, res) => {
+areaRouter.post('/:name', (req, res) => {
   Area.create(req.body).then(() => {
-      res.redirect('/atlanta/areas' + area.name)
+      res.redirect('/atlanta/areas/' + req.params.name)
   });
 });
 
+// =========
+// EDIT AREA
+// =========
+
 areaRouter.get('/area/:areaId/edit', (req, res) => {
-  let area = null;
-  Area.findById(req.params.areaId).then(foundArea => {
-    area = foundArea;
-  }).then(area => {
-    res.render('areas/editAreaForm')
+  Area.findById(req.params.areaId).then(area => {
+    res.render('areas/editAreaForm', { area })
   })
 })
 areaRouter.put('/area/:areaId', (req, res) => {
   Area.findByIdAndUpdate(req.params.areaId, req.body).then(area => {
-    res.redirect('/atlanta/areas/area' + area.id);
+    res.redirect('/atlanta/areas/area/' + req.params.areaId);
   })
 })
+
+// ===========
+// DELETE AREA
+// ===========
 
 areaRouter.delete('/area/:areaId', (req, res) => {
   Area.findByIdAndRemove(req.params.areaId).then(() => {
--- controllers/region.js
+++ controllers/region.js
@@ -2,20 +2,37 @@ const express = require('express')
 const Region = require('../models/Region.js');
 const regionRouter = express.Router()
 
-regionRouter.get('/', (req, res) => {
-    Region.find().then(regions => {
-        res.render('regions/regions', { regions });
-    });
-  });
-  
-  regionRouter.get('/new', (req, res) => {
-    res.render('regions/newRegionForm');
-  });
-  regionRouter.post('/', (req, res) => {
-    Region.create(req.body).then(() => {
-        res.redirect('/atlanta');
-    });
-  });
+// ================
+// INDEX OF REGIONS
+// ================
 
+regionRouter.get('/', (req, res) => {
+  Region.find().then(regions => {
+      res.render('regions/regions', { regions });
+  });
+});
+
+// =================
+// CREATE NEW REGION
+// =================
+
+regionRouter.get('/new', (req, res) => {
+  res.render('regions/newRegionForm');
+});
+regionRouter.post('/', (req, res) => {
+  Region.create(req.body).then(() => {
+    res.redirect('/atlanta');
+  });
+});
+
+// ==================
+// SHOW SINGLE REGION
+// ==================
+
+regionRouter.get('/:regionId', (req, res) => {
+  Region.findById(req.params.regionId).then(region => {
+    res.render('regions/region', { region })
+  })
+})
 
 module.exports = regionRouter;
