3b8e53a56ebc71c6f6c2cc89930a007b7c4e98d5
mekaala <mekaala@gmail.com>
2020-02-28 15:55:16 -0500

    made navbar move as page scrolls down

--- public/script.js
+++ public/script.js
@@ -18,3 +18,17 @@ window.onclick = function(event) {
     about.style.display = "none";
   }
 }
+
+window.onscroll = function() {myFunction()};
+
+const navbar = document.getElementById("navbar");
+
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
